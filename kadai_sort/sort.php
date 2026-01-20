c154987b8601bd34af4aacea0f876adbe44efeee
ik-mametam-1930 <masuko1290@gmail.com>
2026-01-20 13:58:09 +0900

    kakdai_sortをアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $nums = [15, 4, 18, 23, 10 ];
+
+        function sort_2ways(array &$array, bool $order): void
+     {
+    
+        if ($order === true) {
+        echo "昇順にソートします。<br>";
+        sort($array);
+       } else {
+        echo "降順にソートします。<br>";
+        rsort($array);
+       }
+
+   
+        foreach ($array as $value) {
+         echo $value . "<br>";
+       }
+     }
+
+sort_2ways($nums, true);
+
+sort_2ways($nums, false);
+
+
+        ?>
+    </p>
+</body>
+
+</html>
